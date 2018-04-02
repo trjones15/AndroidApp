d15a4160c72af9c4f41f9725090744e77dafa707
Luis Petitjean <petitjean.luis@gmail.com>
2018-04-02 00:00:51 -0500

    merged geocoding and google maps

--- app/src/main/java/com/example/myfirstapp/DisplayMapActivity.java
+++ app/src/main/java/com/example/myfirstapp/DisplayMapActivity.java
@@ -5,7 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -15,29 +22,100 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class DisplayMapActivity extends AppCompatActivity implements OnMapReadyCallback{
 
     private GoogleMap map;
-
+    private final String API_KEY = "AIzaSyD65aKcH2xtNRnxMmwDy4knEu1_GHsaJTk";
+    private String address;
+    private float lng, lat;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        address = getIntent().getStringExtra(MainActivity.EXTRA_MESSAGE);
+
         setContentView(R.layout.activity_display_map);
         MapFragment mapFragment = (MapFragment) getFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
-        MarkerOptions marker = new MarkerOptions();
-        marker.position(new LatLng(29.424122,-98.493628));
-        CameraUpdate newLocation = CameraUpdateFactory.newLatLng(new LatLng(29.424122,-98.493628));
-        map.moveCamera(newLocation);
-        map.addMarker(marker);
+        StringBuilder url = parseStringForUrl(address);
+        getRequest(url);
+    }
+
+    private void updateMapLocation(float lng, float lat) {
+        LatLng coord = new LatLng(lat,lng);
+        CameraUpdate newCamera = CameraUpdateFactory.newLatLng(coord);
         CameraUpdate animate = CameraUpdateFactory.zoomTo(15);
+        MarkerOptions marker = new MarkerOptions();
+        marker.position(coord);
+        map.moveCamera(newCamera);
+        map.addMarker(marker);
         map.animateCamera(animate);
+    }
+
+    private StringBuilder parseStringForUrl(String message) {
+        String[] parsedMessage = message.split(" ");
+        //this should handle the fencepost error
+        StringBuilder request = new StringBuilder();
+        //String request = parsedMessage[0];
+        if(!parsedMessage[0].equals("")) {
+            request.append(parsedMessage[0]);
+            for (int i = 1; i < parsedMessage.length; i++) {
+                request.append("+");
+                request.append(parsedMessage[i]);
+            }
+        } else {
+            request.append("ERROR");
+        }
+
+        StringBuilder beginningOfUrl = new StringBuilder("https://maps.googleapis.com/maps/api/geocode/json?address=");
+        beginningOfUrl.append(request);
+        beginningOfUrl.append("&key=");
+        beginningOfUrl.append(API_KEY);
+
+        return beginningOfUrl;
+    }
+
+    private void getRequest(StringBuilder url) {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.GET, url.toString(),
+                (JSONObject) null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray array = response.getJSONArray("results");
+                            JSONObject results = array.getJSONObject(0);
+                            JSONObject geometry = results.getJSONObject("geometry");
+                            JSONObject location = geometry.getJSONObject("location");
+                            lat = (float) location.getDouble("lat");
+                            lng = (float) location.getDouble("lng");
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    updateMapLocation(lng,lat);
+                                }
+                            });
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //error handling happens here
+            }
+        }
+        );
+        queue.add(jsonRequest);
     }
 }
